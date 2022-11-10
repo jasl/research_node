06bab1c24a3cd5e6c9800c3a190fa7f9d8ecfce5
jasl <jasl9187@hotmail.com>
2022-11-11 05:50:07 +0800

    add few basic tests

--- pallets/computing_workers/src/tests.rs
+++ pallets/computing_workers/src/tests.rs
@@ -2,31 +2,92 @@
 use crate::{mock::*, Error};
 #[allow(unused)]
 use frame_support::{assert_noop, assert_ok, assert_err};
+use frame_system::Account;
+use crate::primitives::WorkerInfo;
+
+#[allow(unused)]
+const ALICE: AccountId = 1;
+#[allow(unused)]
+const ALICE_CONTROLLER: AccountId = 2;
+#[allow(unused)]
+const BOB: AccountId = 3;
+#[allow(unused)]
+const BOB_CONTROLLER: AccountId = 4;
+
+fn register_worker_for(
+	owner: AccountId,
+	controller: AccountId,
+	initial_deposit: Balance
+) -> WorkerInfo<AccountId> {
+	let owner_balance = Balances::free_balance(owner);
+
+	assert_ok!(
+		ComputingWorkers::register(
+			RuntimeOrigin::signed(owner),
+			controller,
+			initial_deposit
+		)
+	);
+
+	let worker_info = ComputingWorkers::workers(controller).unwrap();
+	let current_account = worker_info.current_account;
+
+	assert_eq!(Balances::free_balance(owner), owner_balance - initial_deposit);
+	assert_eq!(Balances::free_balance(controller), 0);
+	assert_eq!(Balances::free_balance(current_account), initial_deposit);
+
+	worker_info
+}
 
 #[test]
 fn register_works() {
 	new_test_ext().execute_with(|| {
-		let owner = 1;
-		let controller = 2;
+		set_balance(ALICE, 101 * DOLLARS, 0);
 
-		set_balance(owner, 101 * DOLLARS, 0);
+		register_worker_for(ALICE, ALICE_CONTROLLER, 100 * DOLLARS);
+
+		run_to_block(1);
+		set_balance(ALICE, 101 * DOLLARS, 0);
+
+		assert_noop!(
+			ComputingWorkers::register(
+				RuntimeOrigin::signed(ALICE),
+				ALICE_CONTROLLER,
+				10 * DOLLARS
+			),
+			Error::<Test>::InitialDepositTooLow
+		);
+
+		assert_noop!(
+			ComputingWorkers::register(
+				RuntimeOrigin::signed(ALICE),
+				ALICE_CONTROLLER,
+				100 * DOLLARS
+			),
+			Error::<Test>::AlreadyRegistered
+		);
+	});
+}
+
+#[test]
+fn deregister_works() {
+	new_test_ext().execute_with(|| {
+		set_balance(ALICE, 101 * DOLLARS, 0);
+
+		let alice_worker = register_worker_for(ALICE, ALICE_CONTROLLER, 100 * DOLLARS);
+		let alice_worker_current_account = alice_worker.current_account;
+
+		run_to_block(1);
 
 		assert_ok!(
-			ComputingWorkers::register(
-				RuntimeOrigin::signed(1),
-				controller,
-				100 * DOLLARS
+			ComputingWorkers::deregister(
+				RuntimeOrigin::signed(ALICE),
+				ALICE_CONTROLLER,
 			)
 		);
 
-		let worker_info = ComputingWorkers::workers(controller).unwrap();
-		let current_account = worker_info.current_account;
-
-		assert_eq!(Balances::free_balance(owner), 1 * DOLLARS);
-		assert_eq!(Balances::free_balance(controller), 0);
-		assert_eq!(Balances::free_balance(current_account), 100 * DOLLARS);
-
-		// Ensure the expected error is thrown when no value is present.
-		// assert_noop!(ComputingWorkers::cause_error(RuntimeOrigin::signed(1)), Error::<Test>::NoneValue);
+		assert_eq!(Balances::free_balance(ALICE), 101 * DOLLARS);
+		assert_eq!(Balances::free_balance(ALICE_CONTROLLER), 0);
+		assert!(!Account::<Test>::contains_key(&alice_worker_current_account));
 	});
 }
