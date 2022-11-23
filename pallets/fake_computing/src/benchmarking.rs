996564ed69d25c03704863e12f4136495f98c652
jasl <jasl9187@hotmail.com>
2022-11-23 13:45:57 +0800

    wip

--- pallets/fake_computing/src/benchmarking.rs
+++ pallets/fake_computing/src/benchmarking.rs
@@ -3,18 +3,18 @@
 use super::*;
 
 #[allow(unused)]
-use crate::Pallet as Template;
+use crate::Pallet as FakeComputing;
 use frame_benchmarking::{benchmarks, whitelisted_caller};
 use frame_system::RawOrigin;
 
 benchmarks! {
-	do_something {
-		let s in 0 .. 100;
-		let caller: T::AccountId = whitelisted_caller();
-	}: _(RawOrigin::Signed(caller), s)
-	verify {
-		assert_eq!(Something::<T>::get(), Some(s));
-	}
+	// do_something {
+	// 	let s in 0 .. 100;
+	// 	let caller: T::AccountId = whitelisted_caller();
+	// }: _(RawOrigin::Signed(caller), s)
+	// verify {
+	// 	assert_eq!(Something::<T>::get(), Some(s));
+	// }
 
-	impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test);
+	impl_benchmark_test_suite!(FakeComputing, crate::mock::new_test_ext(), crate::mock::Test);
 }
